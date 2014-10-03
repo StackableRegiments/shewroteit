a898b8ee6fcb8713e34081e0bd91aa2b459e17e4
Chris Hagan <captain.mister.hagan@gmail.com>
2014-10-03 20:07:12 +1000

    Introduced handlebars

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,7 +14,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
+app.engine('handlebars', exphbs({defaultLayout:"main"}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('flyover.html');
+router.get('/page/:page', function(req, res) {
+  res.render('page', {page:req.params.page});
 });
 
 module.exports = router;
