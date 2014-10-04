e564731d465e208c7543cd42e6f8e4ccec1892da
Chris Hagan <captain.mister.hagan@gmail.com>
2014-10-04 16:04:44 +1000

    Added nav

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/page/:page', function(req, res) {
-  res.render('page', {page:req.params.page});
+    res.render('page', {
+        page:req.params.page,
+        pages:[
+            {id:1,label:"The Web"},
+            {id:2,label:"Web servers"},
+            {id:3,label:"Evaluation"},
+            {id:4,label:"Code"}
+        ]
+    });
 });
 
 module.exports = router;
