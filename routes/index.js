7debb7de2925ad58879ec58ef232c20404feca7c
Chris Hagan <captain.mister.hagan@gmail.com>
2014-10-10 17:46:08 +1100

    chapters

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,10 @@ var stories = (function(){
     var p = author("paper");
     var gp = author("girlPurple");
     var l = author("leer");
-    return [
+    var chapters = [
         {
             id:0,
+            ch:1,
             label:"The Web",
             pages:[
                 {id:0,label:"The Web",comments:[
@@ -23,16 +24,17 @@ var stories = (function(){
                     ]
                 ]},
                 {id:1,label:"The Web server",comments:[
-		    [
-			gp("Is it files?"),
-			p("Yes.  It's pretty much any file.  Anything you can save on your computer you can send down to someone else as a web response.")
-		    ]
-		]},
+                    [
+                        gp("Is it files?"),
+                        p("Yes.  It's pretty much any file.  Anything you can save on your computer you can send down to someone else as a web response.")
+                    ]
+                ]},
                 {id:2,label:"Writing the response"}
             ]
         },
         {
             id:1,
+            ch:2,
             label:"Code",
             pages:[
                 {id:0,label:"Code",comments:[
@@ -57,15 +59,16 @@ var stories = (function(){
                     ]
                 ]},
                 {id:2,label:"More code",comments:[
-		    [
-			p("Biff!  Bam!  Zowie!  Pow!  Tank!"),
-			gp("Dad.")
-		    ]
-		]}
+                    [
+                        p("Biff!  Bam!  Zowie!  Pow!  Tank!"),
+                        gp("Dad.")
+                    ]
+                ]}
             ]
         },
         {
             id:3,
+            ch:3,
             label:"Batman was Sad",
             pages:[
                 {id:0,label:"Batman was sad.",comments:[]},
@@ -78,15 +81,46 @@ var stories = (function(){
                 {id:7,label:"Ennui.",comments:[]}
             ]
         },
-	{
-	    id:5,
-	    label:"Prog Lang",
-	    pages:[
-		{id:0,label:"Languages",comments:[]}
-	    ]
-	},
+        {
+            id:5,
+            ch:4,
+            label:"Prog Lang",
+            pages:[
+                {id:0,label:"Overview",comments:[]},
+                {id:1,label:"Javascript",comments:[
+                    [
+                        gp("Why is Javascript Spiderman?  Oh..."),
+                        p("Tee hee!"),
+                        gp("The web.  It's because the web."),
+                        gp("Oh, Dad.")
+                    ]
+                ]},
+                {id:2,label:"Functions",comments:[
+                    [
+                        gp("Like, the squiggly things?"),
+                        p("Yes.  These: { }.  They indicate a scope.  That is, a place where names make sense."),
+                        gp("What do you mean?"),
+                        p("Like, you know how there's a Victoria in Australia and there's a Victoria in Africa?"),
+                        gp("And the Queen?"),
+                        p("Yes.  And the Queen.  So each time someone says Victoria, you need to know where they want you to be thinking of.  Whether it's Africa, or Canada..."),
+                        gp("Or Queens."),
+                        p("Yes.  Or Queens.")
+                    ]
+                ]},
+                {id:3,label:"Web functions",comments:[]},
+                {id:4,label:"Multiplication",comments:[
+                    [
+                        gp("What language is that other one?"),
+                        p("I'd say it's Erlang, except I'm not sure about variable arity on a function."),
+                        gp("..."),
+                        p("Uh...  About Chapter 8?  I think?")
+                    ]
+                ]}
+            ]
+        },
         {
             id:2,
+            ch:5,
             label:"Crazy Frog",
             pages:[
                 {id:0,label:"Crazy Frog 1",comments:[]},
@@ -98,24 +132,31 @@ var stories = (function(){
         },
         {
             id:4,
+            ch:6,
             label:"Batgirl comes back",
             pages:[
                 {id:0,label:"Batgirl comes back",comments:[]}
             ]
         },
-	{
-	    id:6,
-	    label:"Monads",
-	    pages:[
-		{id:0,label:"1"},
-		{id:1,label:"2"},
-		{id:2,label:"3"},
-		{id:3,label:"4"},
-		{id:4,label:"5"},
-		{id:5,label:"6"}
-	    ]
-	}
-    ]})();
+        {
+            id:6,
+            ch:37,
+            label:"Monads",
+            pages:[
+                {id:0,label:"1"},
+                {id:1,label:"2"},
+                {id:2,label:"3"},
+                {id:3,label:"4"},
+                {id:4,label:"5"},
+                {id:5,label:"6"}
+            ]
+        }
+    ];
+    _.forEach(chapters,function(c){
+        c.chapter = "Chapter "+(c.ch);
+    })
+    return _.sortBy(chapters,"ch");
+})();
 router.get('/', function(req,res) {
     res.redirect('/page/0/0');
 });
